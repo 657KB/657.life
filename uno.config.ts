b355849eae128a9c499af21e32714aa742d9e094
657 <kuyermqi@outlook.com>
2025-08-11 16:49:43 +0800

    update font style of post date

--- uno.config.ts
+++ uno.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     fontFamily: {
       title: ['Snell-Black', 'EarlySummer-Subset', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
       navbar: ['STIX-Italic', 'EarlySummer-Subset', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
-      time: ['Consolas', 'monospace'],
+      time: ['Courier New', 'Courier', 'monospace'],
       serif: ['STIX', 'EarlySummer', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
     },
   },
